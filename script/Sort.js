98cfff563509fe19c7d4bbee9372c448566a7a01
JoshDavZ <145669955+JoshDavZ@users.noreply.github.com>
2026-04-01 10:15:34 +0700

    Update Sort.js

--- script/Sort.js
+++ script/Sort.js
@@ -1,15 +1,20 @@
-function Sort(sort, reference, sortBy, sortReference){
-    // 1. Pre-calculate indices in a Map for O(1) lookup
-    const lookup = new Map(reference.map((row,i) => [row[sortReference], i]));
-    
+// 1. Pre-calculate indices in a Map for O(1) lookup
+function Sort(sorting, reference, sortBy, sortReference = null){
+    let lookup;
+    if (sortReference === null) {
+        lookup = new Map(reference.map((row,i) => [row, i]));
+        console.log('lookup', lookup);
+    }else{
+        lookup = new Map(reference.map((row,i) => [row[sortReference], i]));
+        console.log('lookup', lookup);
+    }
     // 2. Sort using the Map
     // Elements not in the reference array are moved to the end (Infinity)
-    sort.sort((a, b) => {
+    sorting.sort((a, b) => {
         const indexA = lookup.has(a[sortBy]) ? lookup.get(a[sortBy]) : Infinity;
         const indexB = lookup.has(b[sortBy]) ? lookup.get(b[sortBy]) : Infinity;
         return indexA - indexB;
     });
-    console.log(sort);
-    return sort;
-
+    console.log(sorting);
+    return sorting;
 }
